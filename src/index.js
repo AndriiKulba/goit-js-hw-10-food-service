9ab647ddb0c987bb978a201af96069602bfa073e
Andrii Kulba <avk.0149344@gmail.com>
2020-12-05 17:09:18 +0200

    06.12

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import ".css/styles.css";
+import './styles.css';
+import itemTemplate from './templates/food-items.hbs';
+import menu from './menu.json';
+
+const markup = itemTemplate(menu);
+
+const menuRef = document.querySelector('.js-menu');
+
+menuRef.insertAdjacentHTML('beforeend', markup);
+const bodyRef = document.querySelector('body');
+const themeSwitchRef = document.querySelector('.theme-switch__toggle');
+themeSwitchRef.addEventListener('click', () => {
+  bodyRef.classList = 'dark-theme';
+  console.log(bodyRef.classList);
+});
