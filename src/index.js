6c80efc87a09dce017183df924998e7101c706d8
Andrii Kulba <avk.0149344@gmail.com>
2020-12-07 07:12:16 +0200

    доработки

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import './styles.css';
 import itemTemplate from './templates/food-items.hbs';
 import menu from './menu.json';
-
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+localStorage.setItem('theme', JSON.stringify(Theme));
+const saveTheme = localStorage.getItem('theme');
+const parceTheme = JSON.parse(saveTheme);
+const lightTheme = parceTheme.LIGHT;
+const darkTheme = parceTheme.DARK;
 const markup = itemTemplate(menu);
 
 const menuRef = document.querySelector('.js-menu');
@@ -9,7 +17,18 @@ const menuRef = document.querySelector('.js-menu');
 menuRef.insertAdjacentHTML('beforeend', markup);
 const bodyRef = document.querySelector('body');
 const themeSwitchRef = document.querySelector('.theme-switch__toggle');
-themeSwitchRef.addEventListener('click', () => {
-  bodyRef.classList = 'dark-theme';
-  console.log(bodyRef.classList);
-});
+themeSwitchRef.addEventListener('change', handleActiveTheme);
+bodyRef.classList = localStorage.getItem('activeTheme');
+themeSwitchRef.checked = localStorage.getItem('checked');
+
+function handleActiveTheme(e) {
+  if (e.target.checked) {
+    localStorage.setItem('activeTheme', darkTheme);
+    bodyRef.classList = localStorage.getItem('activeTheme');
+    localStorage.setItem('checked', 'true');
+  } else {
+    localStorage.setItem('activeTheme', lightTheme);
+    bodyRef.classList = localStorage.getItem('activeTheme');
+    localStorage.removeItem('checked');
+  }
+}
